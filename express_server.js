e97d0ea66dfb588d790c6aff7047c164fcec391e
Linley Faulkner <linleyjfaulkner@gmail.com>
2016-10-14 03:44:53 +0000

    adding function

--- express_server.js
+++ express_server.js
@@ -2,17 +2,28 @@ var express = require("express");
 var app = express();
 var PORT = process.env.PORT || 8080; // default port 8080
 
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
+
 
 app.set("view engine", "ejs");
-const bodyParser = require("body-parser");
-app.use(bodyParser.urlencoded());
+
 
 var urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
 
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // debug statement to see POST parameters
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+});
 
 app.get("/", (req, res) => {
   res.end("Hello!");
@@ -41,3 +52,32 @@ app.get("/hello", (req, res) => {
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
+
+
+function generateRandomString() {
+
+var stringLength = 6
+var alphaLetter = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z","1","2","3","4","5","6","7","8","9","0"]
+var randomString = ""
+ 
+  for (var i = 0; i < stringLength; i++) {
+    min = 0
+    max = alphaLetter.length
+
+    var ranIndex = Math.floor(Math.random() * (max - min + 1)) + min;
+    var ranValue = alphaLetter[ranIndex]
+
+    randomString += ranValue 
+  }
+
+  return randomString
+
+}
+
+generateRandomString()
+console.log(generateRandomString())
+
+
+
+
+
